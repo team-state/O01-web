661b0aa26ecb17288c9e4f0067e1d4a7ad7572ea
Seunghyo Lee <bingwer@naver.com>
2023-05-21 14:41:06 +0900

    refactor: 에러 분기 처리

--- constants/error.ts
+++ constants/error.ts
@@ -1,1 +1,4 @@
-export const ENV_ERROR_MESSAGE = '환경변수를 확인해 주세요.';
+export const ENV_ERROR_MESSAGE = 'Check environment value.';
+export const AUTH_ERROR_MESSAGE = 'Need login.';
+export const PARAMETER_ERROR = 'Check parameter.';
+export const UNKNOWN_ERROR = 'Unknown error. Check detail Logs.';
--- libs/server/index.ts
+++ libs/server/index.ts
@@ -1,7 +1,6 @@
 import authOptions from './auth';
-import createResponse from './createResponse';
 import prisma from './prismaClient';
 import withRequest from './withRequest';
 import withResponse from './withResponse';
 
-export { authOptions, createResponse, prisma, withRequest, withResponse };
+export { authOptions, prisma, withRequest, withResponse };
