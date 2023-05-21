735fa294936a2700e59eb5626c47643cc55bf588
Seunghyo Lee <bingwer@naver.com>
2023-05-21 21:48:49 +0900

    feat: 카테고리 리스트 조회 API 작성

--- app/api/category/route.ts
+++ app/api/category/route.ts
@@ -1,6 +1,19 @@
-import { PARAMETER_ERROR, UNKNOWN_ERROR } from '@constants/error';
-import { prisma, withRequest, withResponse } from '@libs/server';
-import getUserIdFromSession from 'libs/server/getUserIdFromSession';
+import { INVALID_USER, PARAMETER_ERROR, UNKNOWN_ERROR } from '@constants/error';
+import {
+  prisma,
+  withRequest,
+  withResponse,
+  getParamFromRequest,
+  getUserIdFromSession,
+  getUserIdFromEmail,
+} from '@libs/server';
+
+interface ICategoryListAPIResponse {
+  id: number;
+  name: string;
+  url: string;
+  thumbnailId: string;
+}
 
 const createCategory = async (request: Request) => {
   const userId = await getUserIdFromSession();
@@ -29,5 +42,30 @@ const createCategory = async (request: Request) => {
   if (!response) throw Error(UNKNOWN_ERROR);
 };
 
+const getCategory = async (request: Request) => {
+  const email = getParamFromRequest(request, 'email');
+
+  if (!email) throw Error(PARAMETER_ERROR);
+
+  const userId = await getUserIdFromEmail(email);
+
+  const response = await prisma.category.findMany({
+    where: {
+      userId,
+    },
+    select: {
+      id: true,
+      name: true,
+      thumbnailId: true,
+      url: true,
+    },
+  });
+
+  return response;
+};
+
 export const POST = async (request: Request) =>
   withResponse(withRequest(createCategory)(request));
+
+export const GET = async (request: Request) =>
+  withResponse<ICategoryListAPIResponse[]>(withRequest(getCategory)(request));
