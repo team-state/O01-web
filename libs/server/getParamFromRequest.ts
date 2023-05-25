f09c258c7129c1401522f8c201540cd4b5c0262a
seunghyo Lee <bingwer@naver.com>
2023-05-25 21:46:25 +0900

    refactor: getParamFormRequest 함수에 타입 주입 및 전체 파라미터 반환으로 변경

--- libs/server/getParamFromRequest.ts
+++ libs/server/getParamFromRequest.ts
@@ -1,4 +1,16 @@
-const getParamFromRequest = (request: Request, key: string) =>
-  new URL(request.url).searchParams.get(key);
+import { PARAMETER_ERROR } from '@constants/error';
 
+const getParamFromRequest = <T>(request: Request): T => {
+  const { searchParams } = new URL(request.url);
+
+  if (!searchParams) throw new Error(PARAMETER_ERROR);
+
+  const params: Record<string, string> = {};
+
+  searchParams.forEach((value, key) => {
+    params[key] = value;
+  });
+
+  return params as T;
+};
 export default getParamFromRequest;
